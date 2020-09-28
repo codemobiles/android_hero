0668886bffea26f5fdc23540a41b72c111e78a96
iBlurBlur <tanakorn.ngam@gmail.com>
2020-09-28 16:21:38 +0700

    event handler

--- app/src/main/java/com/codemobiles/androidhero/MainActivity.kt
+++ app/src/main/java/com/codemobiles/androidhero/MainActivity.kt
@@ -2,10 +2,21 @@ package com.codemobiles.androidhero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login_button.setOnClickListener {
+            val username = username_edittext.text.toString()
+            val password: String = password_edittext.text.toString()
+
+            Toast.makeText(applicationContext, "u: $username, p: $password", Toast.LENGTH_LONG)
+                .show()
+        }
     }
+
 }
