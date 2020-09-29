3aa50fef2d6170e64e82f144d78bbba10653b127
iBlurBlur <tanakorn.ngam@gmail.com>
2020-09-29 16:03:29 +0700

    prepare stock recyclerview

--- app/src/main/java/com/codemobiles/androidhero/StockFragment.kt
+++ app/src/main/java/com/codemobiles/androidhero/StockFragment.kt
@@ -5,13 +5,44 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.codemobiles.androidhero.databinding.CustomStockListBinding
+import com.codemobiles.androidhero.databinding.FragmentStockBinding
 
 class StockFragment : Fragment() {
+    private lateinit var binding: FragmentStockBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stock, container, false)
+        binding = FragmentStockBinding.inflate(layoutInflater)
+
+        // important
+        binding.recyclerview.adapter = StockAdapter()
+        binding.recyclerview.layoutManager = GridLayoutManager(context, 2)
+
+        return binding.root
     }
+
+    class StockAdapter : RecyclerView.Adapter<CustomViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+            val binding = CustomStockListBinding.inflate(LayoutInflater.from(parent.context))
+            return CustomViewHolder(binding.root)
+        }
+
+        override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+
+        }
+
+        override fun getItemCount() = 100
+
+    }
+
+    class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+
 }
