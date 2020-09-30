e3c9637188b0017a7f32196f2b3b63acaf810352
iBlurBlur <tanakorn.ngam@gmail.com>
2020-09-30 15:49:55 +0700

    open camera

--- app/src/main/java/com/codemobiles/androidhero/FormActivity.kt
+++ app/src/main/java/com/codemobiles/androidhero/FormActivity.kt
@@ -4,17 +4,35 @@ import android.Manifest
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.codemobiles.androidhero.databinding.ActivityFormBinding
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import pl.aprilapps.easyphotopicker.ChooserType
+import pl.aprilapps.easyphotopicker.EasyImage
+import java.io.File
 
 
 class FormActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFormBinding
+    private var easyImage: EasyImage? = null
+    private var file: File? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form)
+        binding = ActivityFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        easyImage = EasyImage.Builder(this)
+            .setChooserTitle("Pick media")
+            .setCopyImagesToPublicGalleryFolder(false)
+            .setChooserType(ChooserType.CAMERA_AND_GALLERY)
+            .setFolderName("EasyImage sample")
+            .allowMultiple(true)
+            .build()
 
         checkRuntimePermission()
     }
@@ -27,6 +45,7 @@ class FormActivity : AppCompatActivity() {
             ).withListener(object : MultiplePermissionsListener {
                 override fun onPermissionsChecked(report: MultiplePermissionsReport) { /* ... */
                     if (report.areAllPermissionsGranted()) {
+                        easyImage?.openCameraForImage(this@FormActivity)
                         Toast.makeText(applicationContext, "####", Toast.LENGTH_SHORT).show()
                     } else {
                         //todo
