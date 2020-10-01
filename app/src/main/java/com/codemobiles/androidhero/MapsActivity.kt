c1d2b0896de73035b0e17b0ac4e922711b3749c3
iBlurBlur <tanakorn.ngam@gmail.com>
2020-10-01 14:39:08 +0700

    tracking location

--- app/src/main/java/com/codemobiles/androidhero/MapsActivity.kt
+++ app/src/main/java/com/codemobiles/androidhero/MapsActivity.kt
@@ -2,9 +2,13 @@ package com.codemobiles.androidhero
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.codemobiles.androidhero.databinding.ActivityMapsBinding
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
@@ -16,13 +20,22 @@ import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
+private const val FASTEST_INTERVAL: Long = 1000
+private const val UPDATE_INTERVAL: Long = 3000
+
 class MapsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMapsBinding
     private lateinit var mMap: GoogleMap
 
+    private var mCurrentLocation: Location? = null
+    private var mLocationProvider: FusedLocationProviderClient? = null
+    private var mCallback: LocationCallback? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        mLocationProvider = LocationServices.getFusedLocationProviderClient(this)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -60,6 +73,7 @@ class MapsActivity : AppCompatActivity() {
                 override fun onPermissionsChecked(report: MultiplePermissionsReport) { /* ... */
                     if (report.areAllPermissionsGranted()) {
                         mMap.isMyLocationEnabled = true
+                        trackLocation()
                     }
                 }
 
@@ -70,6 +84,30 @@ class MapsActivity : AppCompatActivity() {
                     token!!.continuePermissionRequest()
                 }
             }).check()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun trackLocation() {
+        val request = LocationRequest()
+        request.interval = UPDATE_INTERVAL
+        request.fastestInterval = FASTEST_INTERVAL
+        request.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        mCallback = object : LocationCallback() {
+            override fun onLocationResult(resut: LocationResult?) {
+                super.onLocationResult(resut)
+                val currentLocation = resut!!.lastLocation
+                if (currentLocation != null) {
+                    mCurrentLocation = currentLocation
+                    Log.d("my_location", "${mCurrentLocation!!.latitude}")
+                    val latLng = LatLng(mCurrentLocation!!.latitude, mCurrentLocation!!.longitude)
+                    addMarker(latLng)
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18f))
+                }
+            }
+        }
+
+        mLocationProvider!!.requestLocationUpdates(request, mCallback, Looper.myLooper())
     }
 
 }
